3cd064ece3bede41a5296f7a04326dc72fde962f
SparkZhou <zhoujuhui@qq.com>
2019-01-10 22:07:08 +0800

    delete the file

--- src/main/java/spark/smartonlinecourse/dao/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package spark.smartonlinecourse.dao;
-
-import spark.smartonlinecourse.entity.User;
-
-public interface UserMapper {
-    User selectByEmail(String email);
-    Integer updateUser(User user);
-    Integer insertUser(User user);
-}
