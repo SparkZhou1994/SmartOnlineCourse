14da9ffc5d7b9872b7fabb919e94cf4ec8635923
SparkZhou <zhoujuhui@qq.com>
2019-01-15 18:46:44 +0800

    config eureka TODO

---
+++ eureka-config-center/src/main/java/spark/course/EurekaConfigCenterApplication.java
@@ -0,0 +1,22 @@
+package spark.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @ClassName EurekaConfigCenterApplication
+ * @Description TODO
+ * @Author Spark
+ * @Date 1/15/2019 1:56 PM
+ * @Version 1.0
+ **/
+@EnableEurekaServer
+@EnableConfigServer
+@SpringBootApplication
+public class EurekaConfigCenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaConfigCenterApplication.class, args);
+    }
+}
