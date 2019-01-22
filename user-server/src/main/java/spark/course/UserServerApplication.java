eede7ad435cb0c892048937e5c359aa74f56b456
SparkZhou <zhoujuhui@qq.com>
2019-01-22 16:20:49 +0800

    Add User Module

---
+++ user-server/src/main/java/spark/course/UserServerApplication.java
@@ -0,0 +1,11 @@
+package spark.course;
+
+/**
+ * @ClassName UserServerApplication
+ * @Description TODO
+ * @Author Spark
+ * @Date 1/21/2019 1:07 PM
+ * @Version 1.0
+ **/
+public class UserServerApplication {
+}
---
+++ web-invoker/src/main/java/spark/course/WebInvokerApplication.java
@@ -0,0 +1,11 @@
+package spark.course;
+
+/**
+ * @ClassName WebInvokerApplication
+ * @Description TODO
+ * @Author Spark
+ * @Date 1/21/2019 11:03 AM
+ * @Version 1.0
+ **/
+public class WebInvokerApplication {
+}
