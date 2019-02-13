73ed8ab59af0e3e270537e42de7b8e98f90a9a4d
SparkZhou <zhoujuhui@qq.com>
2019-02-13 16:48:39 +0800

    Add the courseWare add Message

--- web-invoker/src/main/java/spark/course/service/aspect/MessageServiceAspect.java
+++ web-invoker/src/main/java/spark/course/service/aspect/MessageServiceAspect.java
@@ -12,6 +12,7 @@ import spark.course.api.FeignCourseApi;
 import spark.course.api.FeignMessageApi;
 import spark.course.entity.bo.CourseBO;
 import spark.course.entity.bo.MessageBO;
+import spark.course.entity.vo.CourseWareVO;
 import spark.course.entity.vo.HomeworkVO;
 import spark.course.entity.vo.MessageVO;
 import spark.course.error.BusinessException;
@@ -38,8 +39,13 @@ public class MessageServiceAspect {
     @Autowired
     FeignCourseApi courseService;
     private final String homeworkReleasePoint = "execution(* spark.course.web.controller.HomeworkController.insert(..))";
+    private final String courseWareReleasePoint = "execution(* spark.course.web.controller.CourseWareController.insert(..))";
+
     @Pointcut(homeworkReleasePoint)
     public void homeworkRelease() {}
+
+    @Pointcut(courseWareReleasePoint)
+    public void courseWareRelease() {}
 
     @After("homeworkRelease()")
     public void doAfterHomeworkRelease(JoinPoint joinPoint) throws BusinessException {
@@ -60,6 +66,31 @@ public class MessageServiceAspect {
                     MessageVO messageVO = new MessageVO();
                     messageVO.setChooseCourseId(courseBOTemp.getChooseCourseId());
                     messageVO.setContent(courseBOWithCourseDetail.getCourseName() + ",有了一份新作业");
+                    String result = messageService.insert(convertToBO(messageVO));
+                    if (result.contains("true")) {
+                        throw new BusinessException(new ClassBusinessError(result));
+                    }
+                }
+            }
+        }
+    }
+
+    @After("courseWareRelease()")
+    public void doAfterCourseWareRelease(JoinPoint joinPoint) throws BusinessException {
+        //get the param
+        Object[] objects = joinPoint.getArgs();
+        for (Object object:objects) {
+            if (object instanceof CourseWareVO) {
+                CourseWareVO courseWareVO = (CourseWareVO)object;
+                //get the courseName
+                CourseBO courseBOWithCourseDetail = JsonUtil.json2Bean(courseService.selectByCourseId(courseWareVO.getCourseId()), CourseBO.class);
+                //get the chooseCourseId list by courseId
+                List<CourseBO> courseBOList = JsonUtil.json2List(chooseCourseService.
+                        selectByCourseId(courseWareVO.getCourseId()), CourseBO.class);
+                for (CourseBO courseBO:courseBOList) {
+                    MessageVO messageVO = new MessageVO();
+                    messageVO.setChooseCourseId(courseBO.getChooseCourseId());
+                    messageVO.setContent(courseBOWithCourseDetail.getCourseName() + ",有了一份新课件");
                     String result = messageService.insert(convertToBO(messageVO));
                     if (result.contains("true")) {
                         throw new BusinessException(new ClassBusinessError(result));
